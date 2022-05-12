dcb7a3284815a18b59571d842fdd14f54d96d29f
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-05-12 15:34:26 +0200

    correct spelling and phrasing in about section

--- content/about.php
+++ content/about.php
@@ -2,6 +2,6 @@
     <h3>Qui suis-je ?</h3>
     <p>Bonjour à tous !</p>
     <p>Je m'appelle Alexis et je suis développeur web, un métier pour lequel je me suis passionné il y a maintenant quelques années.</p>
-    <p>Après avoir d'abord commencé à faire des petits projets de mon côté grâce à des sites comme OpenClassrooms, j'ai sauté le pas et fait une première formation certifiante de Web Designer en 2017, lorsque j'habitais alors en Italie.</p>
-    <p>Aujourd'hui, après une belle carrière de près de dix ans dans le tourisme, j'ai entrepris de former à distance avec le groupe Studi, afin d'obtenir le diplôme de Graduate Développeur Web et Web Mobile, spécialité PHP/Symfony.</p>
+    <p>J'ai d'abord commencé à faire des petits projets en autodidacte, grâce à des sites comme OpenClassrooms, puis j'ai sauté le pas et fait une première formation certifiante de Web Designer en 2017, lorsque j'habitais alors en Italie.</p>
+    <p>Aujourd'hui, après une belle carrière de près de dix ans dans le tourisme, j'ai entrepris de me former à distance avec le groupe Studi, afin d'obtenir le diplôme de Graduate Développeur Web et Web Mobile, spécialité PHP/Symfony.</p>
 </div>
