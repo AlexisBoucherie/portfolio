02a69030c8583a57a75c8d4222c3ea2ac5a63516
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-05-12 15:31:31 +0200

    change about section

--- content/about.php
+++ content/about.php
@@ -2,6 +2,6 @@
     <h3>Qui suis-je ?</h3>
     <p>Bonjour à tous !</p>
     <p>Je m'appelle Alexis et je suis développeur web, un métier pour lequel je me suis passionné il y a maintenant quelques années.</p>
-    <p>Après avoir d'abord commencé à faire des petits projets de mon côté grâce à des sites comme Openclassroom, j'ai sauté le pas et fait une première formation certifiante de Web Designer en 2017, lorsque j'habitais alors en Italie.</p>
-    <p>Aujourd'hui, j'ai mis fin à une carrière de près de dix ans dans le tourisme pour me former à distance, avec le groupe Studi, pour obtenir un diplôme reconnu par l'état de Graduate Développeur Web et Web Mobile, spécialité PHP/Symfony.</p>
+    <p>Après avoir d'abord commencé à faire des petits projets de mon côté grâce à des sites comme OpenClassrooms, j'ai sauté le pas et fait une première formation certifiante de Web Designer en 2017, lorsque j'habitais alors en Italie.</p>
+    <p>Aujourd'hui, après une belle carrière de près de dix ans dans le tourisme, j'ai entrepris de former à distance avec le groupe Studi, afin d'obtenir le diplôme de Graduate Développeur Web et Web Mobile, spécialité PHP/Symfony.</p>
 </div>
