1e663ac1f1093a1c85e4e1404740c3aa0e04f713
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-05-26 10:40:14 +0200

    add projects js-calculator and guess-the-number in achievements.php

--- content/achievements.php
+++ content/achievements.php
@@ -8,13 +8,14 @@
             <h4>CV - Feuille de personnage</h4>
             <a href="https://alexis-boucherie-cv.herokuapp.com/"><div class="project-pic pic1"></div></a>
             <p>Ce projet réalisé de manière libre était un moyen pour moi d'allier mes deux passions : l'informatique et le jeu de rôle. J'ai alors entrepris de confectionner un Curriculum Vitae qui prendrait la forme d'une feuille de personnage inspirée du jeu Donjons et Dragons.</p>
-            <h5>Outils et technologies utilisés :</h5>
+            <h5>Langages et outils utilisés :</h5>
             <ul>
                 <li>Figma (wireframe et mockup)</li>
-                <li>HTML5 / CSS3</li>
-                <li>CSS : grid et flexbox</li>
+                <li>HTML5</li>
+                <li>CSS3 (grid, flexbox)</li>
                 <li>Media queries</li>
-                <li>Google Fonts</li>
+                <li>Intégration Google Fonts</li>
+                <li>Déploiement : Heroku</li>
             </ul>
         </div>
             
@@ -22,14 +23,14 @@
             <h4>Les Krokettes du Youki</h4>
             <a href="https://leskrokettesduyouki.netlify.app/index.html"><div class="project-pic pic2"></div></a>
             <p>Ce projet réalisé dans le cadre d'une toute première évaluation de diplôme présente le site d'une association fictive. La consigne ici était de réaliser un site mobile first statique et qui comprenait plusieurs pages, puis de le déployer.</p>
-            <h5>Outils et technologies requis :</h5>
+            <h5>Langages et outils utilisés :</h5>
             <ul>
-                <li>HTML5 / CSS3</li>
-                <li>CSS : grid et flexbox</li>
+                <li>HTML5</li>
+                <li>CSS3 (grid, flexbox)</li>
                 <li>Media queries</li>
                 <li>Page événement</li>
-                <li>Formulaire (brut)</li>
-                <li>Heroku</li>
+                <li>Formulaire (HTML seul)</li>
+                <li>Déploiement : Heroku</li>
             </ul>
         </div>
           
@@ -37,15 +38,46 @@
             <h4>Service à la Personne, Vars</h4>
             <a href="https://service-personne-16.herokuapp.com/"><div class="project-pic pic3"></div></a>
             <p>Ce site responsif a été réalisé en concertation avec un particulier en autoentreprise. Il s'agit d'un site vitrine qui permet de faire la promotion des services réalisés par Service Personne Vars et qui permet aux client de contacter l'entreprise.</p>
-            <h5>Outils et technologies utilisés :</h5>
+            <h5>Langages et outils utilisés :</h5>
             <ul>
                 <li>Figma (wireframe et mockup)</li>
-                <li>HTML5 / CSS3</li>
-                <li>CSS : grid et flexbox</li>
+                <li>HTML5</li>
+                <li>CSS3 (grid, flexbox)</li>
                 <li>Media queries</li>
                 <li>Formulaire de contact</li>
-                <li>PHP</li>
-                <li>Heroku</li>
+                <li>PHP ($_POST)</li>
+                <li>Déploiement : Heroku</li>
+            </ul>
+        </div>
+
+        <div class="project box">
+            <h4>Outil : JS simple calculator</h4>
+            <a href="https://service-personne-16.herokuapp.com/"><div class="project-pic pic3"></div></a>
+            <p>Cette calculatrice réalisée en Vanilla JS est un outil simple qui m'a permis de réaliser des opérations simples en terme de manipulation du DOM. Comme axes d'amélioration, on pourrait la rendre mieux responsive, ajouter une touche pour les calculs à virgules et créer des boucles pour rendre le code moins répétitif. Cependant, elle marche très bien en l'état.</p>
+            <h5>Langages et outils utilisés :</h5>
+            <ul>
+                <li>JavaScript :</li>
+                <ul>
+                    <li>selectElementById</li>
+                    <li>addEventListener</li>
+                    <li>parseInt</li>
+                </ul>
+                <li>Déploiement : Heroku</li>
+            </ul>
+        </div>
+
+        <div class="project box">
+            <h4>Mini jeu : Devine le nombre !</h4>
+            <a href="https://service-personne-16.herokuapp.com/"><div class="project-pic pic3"></div></a>
+            <p>Avec ce petit jeu, il s'agit de trouver un nombre aléatoire, généré par l'ordinateur, entre 1 et 100 en moins de dix essais.</p>
+            <h5>Langages et outils utilisés :</h5>
+            <ul>
+                <li>JavaScript :</li>
+                <ul>
+                    <li>selectElementById</li>
+                    <li>addEventListener</li>
+                </ul>
+                <li>Déploiement : Heroku</li>
             </ul>
         </div>
 
