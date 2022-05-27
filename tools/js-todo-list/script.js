db00b54382d95859c0cff9cad18c5d0573928179
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-05-27 10:52:34 +0200

    add filter options for todos

--- tools/js-todo-list/script.js
+++ tools/js-todo-list/script.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //LISTENERS
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //FUNCTIONS
 function addTodo(event) {
@@ -72,4 +74,37 @@ function deleteCheck(e) {
         //on change la classe de l'élément parent à chaque fois qu'on clique (avec toggle, comme ON/OFF)
         todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e) {
+    /* on cible les enfants de la todoList, donc les todos */
+    const todos = todoList.childNodes;
+    /* on boucle sur les todos */
+    todos.forEach(function (todo) { 
+        const mStyle = todo.style;  
+        if(mStyle != undefined && mStyle != null){
+            switch (e.target.value) {
+                case "all":
+                    mStyle.display = "flex";
+                    break;
+                /* si la classe de la todo est "completed" */
+                case "completed":
+                    if (todo.classList.contains('completed')) {
+                        mStyle.display = 'flex';
+                    } else {
+                        mStyle.display = "none";
+                    }
+                    break;
+                /* si la classe de la todo n'est pas "completed" */
+                case "uncompleted":
+                    if (todo.classList.contains('completed')){
+                        mStyle.display = 'none';
+                    }
+                    else{
+                        mStyle.display = "flex";
+                    }
+                    break;
+            }
+        }
+    })
 }
