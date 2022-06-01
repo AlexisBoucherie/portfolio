3c1090a7f6c6ee817002fcaf6380d0231d26f85b
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-06-01 18:14:02 +0200

    tru to build new calculator, but found new bugs

--- tools/js-vanilla-calculator/script.js
+++ tools/js-vanilla-calculator/script.js
@@ -1,114 +1,96 @@
-//on cible l'écran d'affichage
-let screen = document.getElementById("screen");
+/* VARIABLES */
 
-//on cible l'écran caché pour mémoriser la première partie du calcul (firstNumber)
-let remember = document.getElementById("memory");
+// on cible les écrans
+const memoryScreen = document.getElementById("memory");
+const displayScreen = document.getElementById("screen");
+// on cible les boutons
+const numberButtons = document.querySelectorAll(".number-btn");
+const signButton = document.querySelectorAll(".sign-btn");
+const acButton = document.getElementById("all-clear");
+const equalButton = document.getElementById("equal");
+// on cible le type d'opération
+let operation = document.getElementById("operation-type");
 
-//on cible un autre écran pour se souvenir du type d'opération
-let operationType = document.getElementById("opType");
+/* LISTENERS */
 
-//on cible toutes les touches une par une
-let numberZero = document.getElementById("zero");
-let numberOne = document.getElementById("one");
-let numberTwo = document.getElementById("two");
-let numberThree = document.getElementById("three");
-let numberFour = document.getElementById("four");
-let numberFive = document.getElementById("five");
-let numberSix = document.getElementById("six");
-let numberSeven = document.getElementById("seven");
-let numberEight = document.getElementById("eight");
-let numberNine = document.getElementById("nine");
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        let number = button.innerText;
+        appendNumber(number);
+    })
+});
 
-let plusSign = document.getElementById("plus");
-let minusSign = document.getElementById("minus");
-let timesSign = document.getElementById("times");
-let dividedbySign = document.getElementById("dividedby");
-let equalSign = document.getElementById("equal");
-let clearCalc = document.getElementById("clear");
+signButton.forEach(button => {
+    button.addEventListener("click", () => {
+        operation.innerText = button.innerText;
+        chooseOperation();
+    })
+});
 
-//on crée des événements pour afficher les numéros saisis à l'écran
-numberZero.addEventListener("click", () => {reset(); screen.innerHTML += 0;});
-numberOne.addEventListener('click', () => {reset(); screen.innerHTML += 1;});
-numberTwo.addEventListener('click', () => {reset(); screen.innerHTML += 2;});
-numberThree.addEventListener('click', () => {reset(); screen.innerHTML += 3;});
-numberFour.addEventListener('click', () => {reset(); screen.innerHTML += 4;});
-numberFive.addEventListener('click', () => {reset(); screen.innerHTML += 5;});
-numberSix.addEventListener('click', () => {reset(); screen.innerHTML += 6;});
-numberSeven.addEventListener('click', () => {reset(); screen.innerHTML += 7;});
-numberEight.addEventListener('click', () => {reset(); screen.innerHTML += 8;});
-numberNine.addEventListener('click', () => {reset(); screen.innerHTML += 9;});
+equalButton.addEventListener("click", () => {
+    calculate();
+});
 
-//on crée des événements pour les touches des signes opératoires
-plusSign.addEventListener('click', () => {memorize(); add(); clear();});
-minusSign.addEventListener('click', () => {memorize(); substract(); clear();});
-dividedbySign.addEventListener('click', () => {memorize(); divide(); clear();});
-timesSign.addEventListener('click', () => {memorize(); multiply(); clear();});
-equalSign.addEventListener('click', equals);
-clearCalc.addEventListener('click', clearAll);
+acButton.addEventListener("click", () => {
+    clear();
+});
 
-// --- OPERATIONS --- //
-
-//on crée une fonction pour se souvenir de ce que l'on a tapé
-function memorize() {
-    remember.innerHTML += screen.innerHTML;
-}
-
-//on crée une fonction pour les additions
-function add() {
-    operationType.innerHTML = "addition";
-}
-
-//on crée une fonction pour les soustractions
-function substract() {
-    operationType.innerHTML = "substraction";
-}
-
-//on crée une fonction pour les multiplications
-function multiply() {
-    operationType.innerHTML = "multiplication";
-}
-
-//on crée une fonction pour les divisions
-function divide() {
-    operationType.innerHTML = "division";
-}
-
-//on crée une fonction pour remettre l'écran à zéro
+/* FUNCTIONS */
 function clear() {
-    screen.innerHTML = "";
+    memoryScreen.innerText = "";
+    displayScreen.innerText = "";
+    operation.innerText = "";
 }
 
-//on crée une fonction pour faire un reset de la calculette
-function clearAll() {
-    screen.innerHTML = "";
-    remember.innerHTML = "";
-    operationType.innerHTML = "";
-}
-
-//on crée une fonction pour remettre la calculatrice à zéro si on a déjà effectué une opé
-function reset() {
-    if (operationType.innerHTML === "over" || screen.innerHTML === "ERROR") {
-        clearAll();
+function appendNumber(number) {
+    // on vérifie s'il n'y a pas déjà un point
+    if (number === "." && displayScreen.innerText.includes(".")) return;
+    // si on commence par taper un point, on considère que le premier chiffre est un zéro
+    if (number === "." && displayScreen.innerText === "") {
+        displayScreen.innerText = "0"
     }
+    displayScreen.innerText += number;
 }
 
-// --- RESULTAT --- //
-
-// on crée une fonction pour calculer les opérations suivant leur type (touche égal)
-function equals() {
-    let firstNumber = remember.innerHTML;
-    let secondNumber = screen.innerHTML;
-    if(operationType.innerHTML === "addition") {
-        screen.innerHTML = parseInt(firstNumber) + parseInt(secondNumber);
-    } else if(operationType.innerHTML === "substraction") {
-        screen.innerHTML = parseInt(firstNumber) - parseInt(secondNumber);
-    } else if(operationType.innerHTML === "multiplication") {
-        screen.innerHTML = parseInt(firstNumber) * parseInt(secondNumber);
-    } else if(operationType.innerHTML === "division") {
-        screen.innerHTML = parseInt(firstNumber) / parseInt(secondNumber);
-    } else {
-        screen.innerHTML = "ERROR";
+function chooseOperation() {
+    // on vérifie que l'écran n'est pas vide, auquel cas la fonction ne s'exécute pas
+    if (displayScreen === "") return;
+    // s'il y a déjà quelque chose en mémoire, on exécute le calcul
+    if (memoryScreen !== "") {
+        calculate();
     }
-    //on change le type sur "over" pour pouvoir exécuter un clearAll()
-    operationType.innerHTML = "over";
+    // on charge l'écran d'affichage dans l'écran de mémorisation 
+    memoryScreen.innerText = displayScreen.innerText;
+    // on remet à zéro l'écran d'affichage
+    displayScreen.innerText = "";
+}
+
+function calculate() {
+    let operationType = operation.innerText;
+    // si aucun opérateur a  été pressé, on bloque la focntion
+    if (operation.innerText === "") return;
+
+    const memory = parseFloat(memoryScreen.innerText);
+    const display = parseFloat(displayScreen.innerText);
+    // s'il manque une des parties de l'opération, on bloque la fonction
+    if (isNaN(memory) || isNaN(display)) return;
+    switch (operationType) {
+        case "+":
+            computation = memory + display;
+            break;
+        case "-":
+            computation = memory - display;
+            break;
+        case "x":
+            computation = memory * display;
+            break;
+        case "/":
+            computation = memory / display;
+            break;
+        default:
+            return;
+    }
+    displayScreen.innerText = computation;
+    operation.innerText = "";
+    memoryScreen.innerText = "";
 }
